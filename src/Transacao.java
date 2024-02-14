e34ad44386ec062f59ce0a4cb2769076701f73b3
Eduardo Silva <thiago.novak7@gmail.com>
2024-02-14 19:54:00 +0000

    transacao

--- src/Transacao.java
+++ src/Transacao.java
@@ -1,4 +1,14 @@
 public class Transacao {
+    private double valor;
+    private Date data;
+    private String categoria;
+    private String descricao;
+
+    public Transacao(double valor, Date data, String categoria, String descricao){
+        this.valor = valor;
+        thiss.data = data;
+    }
+
     public double getValor() {
         return 0;
     }
