8eac26f044f5bf621c9b96e9d342f6eebb81532f
Eduardo Silva <thiago.novak7@gmail.com>
2024-02-16 22:16:36 +0000

    get e seters de Transacao

--- src/Transacao.java
+++ src/Transacao.java
@@ -6,10 +6,41 @@ public class Transacao {
 
     public Transacao(double valor, Date data, String categoria, String descricao){
         this.valor = valor;
-        thiss.data = data;
+        this.data = data;
+        this.categoria = categoria;
+        this.descricao = descricao;
     }
 
     public double getValor() {
         return 0;
     }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
 }
