c2e000b033daf8216e7da373c1025df676e9cb71
eduardonk9999 <thiago.novak7@gmail.com>
2024-02-19 11:11:09 -0300

    feat: class Categoria

--- src/Categoria.java
+++ src/Categoria.java
@@ -1,2 +1,15 @@
 public class Categoria {
+    private String nome;
+
+    public Categoria(String nme){
+        this.nome nome;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
 }
